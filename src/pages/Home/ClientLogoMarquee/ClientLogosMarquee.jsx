93fcc5b76d37fe3a951708fcabc54544892838bf
shahadot99999 <shahadot99999@gmail.com>
2026-01-11 18:52:51 +0600

    Use Animate on Scroll

--- src/pages/Home/ClientLogoMarquee/ClientLogosMarquee.jsx
+++ src/pages/Home/ClientLogoMarquee/ClientLogosMarquee.jsx
@@ -1,6 +1,6 @@
 import Marquee from "react-fast-marquee";
 
-import logo1 from "../../../assets/assets/brands/amazon.png";
+import logo1 from  "../../../assets/assets/brands/amazon.png";
 import logo2 from "../../../assets/assets/brands/amazon_vector.png";
 import logo3 from "../../../assets/assets/brands/casio.png";
 import logo4 from "../../../assets/assets/brands/moonstar.png";
@@ -13,17 +13,17 @@ const logos = [logo1, logo2, logo3, logo4, logo5, logo6, logo7];
 const ClientLogosMarquee = () => {
   return (
     <div className="py-14 bg-base-100">
-      <h2 className="text-center  font-bold mb-8">
+      <h2 className="text-center text-primary font-bold mb-12">
         We've helped thousands of sales teams
       </h2>
 
       <Marquee speed={50} pauseOnHover gradient={false}>
         {logos.map((logo, index) => (
-          <div key={index} className="mx-12 flex items-center">
+          <div key={index} className="mx-24 flex items-center">
             <img
               src={logo}
               alt="Client Logo"
-              className="h-4 object-contain grayscale hover:grayscale-0 transition"
+              className="h-6 object-contain grayscale hover:grayscale-0 transition"
             />
           </div>
         ))}
