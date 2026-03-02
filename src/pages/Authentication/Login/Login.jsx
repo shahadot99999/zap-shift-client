cf013d2ad68deed2b4963b100826f0801077a73e
shahadot99999 <shahadot99999@gmail.com>
2026-03-02 17:37:46 +0600

    Register form using react hook form and handle form error

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 
 const Login = () => {
 
-    const {register, handleSubmit}=useForm();
+    const {register, handleSubmit, formState:{errors}}=useForm();
 
     const onSubmit = data =>{
         console.log(data);
@@ -24,12 +24,25 @@ const Login = () => {
                         required: true,
                         minLength: 6})}                    
                      className="input" placeholder="Password" />
+                     {
+                        errors.password?.type === 'required' && <p 
+                        className='text-red-500' >Password is required
+                        </p>
+                     }
+                     {
+                        errors.password?.type === 'minLength' && <p
+                        className='text-red-500'>
+                            Password Must be 6 characters or longer
+                        </p>
+                     }
 
 
                     <div><a className="link link-hover">Forgot password?</a></div>
 
-                    <button className="btn btn-neutral mt-4">Login</button>
+                    
                 </fieldset>
+
+                <button className="btn btn-neutral mt-4">Login</button>
             </form>
         </div>
     );
