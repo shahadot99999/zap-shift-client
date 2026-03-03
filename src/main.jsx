4af5a66890099f710b1bcdb5481798a5b6efda34
shahadot99999 <shahadot99999@gmail.com>
2026-03-03 12:54:20 +0600

    Created firebase project and auth context structure

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import { router } from './router/router.jsx';
 
 import 'aos/dist/aos.css';
 import Aos from 'aos';
+import AuthProvider from './contexts/AuthContext/AuthProvider.jsx';
 
 Aos.init();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='font-urbanist max-w-7xl mx-auto'>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
      
   </StrictMode>,
