3ce9f1b7ef2026bd3d8634357ce06622c79e18ca
shahadot99999 <shahadot99999@gmail.com>
2026-03-12 10:13:51 +0600

    Search service center location in the map

--- src/pages/Coverage/BangladeshMap.jsx
+++ src/pages/Coverage/BangladeshMap.jsx
@@ -1,27 +1,61 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 // Bangladesh center coordinates
 const position = [23.6850, 90.3563];
 
-const BangladeshMap = () => {
+// Helper component to move map
+function FlyToDistrict({ coords }) {
+const map = useMap();
+if (coords) {
+map.flyTo(coords, 14, { duration: 1.5 });
+}
+return null;
+}
+
+const BangladeshMap = ({serviceCenters}) => {
+
+    const [searchText, setSearchText] = useState('');
+    const [activeCoords, setActiveCoords] = useState(null);
+    const [activeDistrict, setActiveDistrict] = useState(null);
+    
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const district = serviceCenters.find(d =>
+            d.district.toLowerCase().includes(searchText.toLowerCase())
+        );
+        if (district) {
+            setActiveCoords([district.latitude, district.longitude]);
+            setActiveDistrict(district.district);
+        };
+    }
+
     return (
         <div>
             {/* Search box (UI only for now) */}
-            <div className="flex justify-center mb-8">
-                <input
+            <form
+            onSubmit={handleSearch}
+            className='absolute top-4 left-1/2 transform -translate-x-1/2 
+            z[1000] w-full max-w-md px-4 flex'
+            >
+              <input
                     type="text"
                     placeholder="Search district..."
                     className="input input-bordered w-full max-w-md"
+                    value={searchText}
+                    onChange={(e)=>setSearchText(e.target.value)}
                 />
+            </form>
+            <div className="flex justify-center mb-8">
+                
             </div>
 
             {/* Map */}
-            <div className="h-[500px] w-full rounded-xl overflow-hidden">
+            <div className="h-[800px] w-full rounded-xl overflow-hidden">
                 <MapContainer
                     center={position}
-                    zoom={7}
+                    zoom={8}
                     scrollWheelZoom={true}
                     className="h-full w-full"
                 >
@@ -31,12 +65,29 @@ const BangladeshMap = () => {
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
 
+                    <FlyToDistrict coords={activeCoords} />
+
                     {/* Marker */}
-                    <Marker position={position}>
+                    {/* <Marker position={position}>
                         <Popup>
                             Our Delivery Coverage <br /> Bangladesh
                         </Popup>
-                    </Marker>
+                    </Marker> */}
+                    {
+                       serviceCenters.map((center, index) =><Marker 
+                       key={index}
+                       position={[center.latitude, center.longitude]}
+                     >
+                        <Popup  autoOpen={center.district === activeDistrict}
+                        
+                        >
+                            <strong>{center.district}</strong><p>
+                                {center.covered_area.join(',')}
+                            </p>
+                            Our Delivery Coverage <br /> Bangladesh
+                        </Popup>
+                    </Marker> ) 
+                    }
                 </MapContainer>
             </div>
         </div>
--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import BangladeshMap from './BangladeshMap';
+import { useLoaderData } from 'react-router';
 
 
 const Coverage = () => {
+
+    const serviceCenters = useLoaderData();
+    //console.log(serviceCenters);
     return (
         <div className='max-w-4xl mx-auto px-4 py-10'>
             <h1>We are available in 64 district</h1>
-            <BangladeshMap/>
+            <BangladeshMap  serviceCenters={serviceCenters}/>
         </div>
     );
 };
